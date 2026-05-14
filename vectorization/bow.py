8caa359c0d11bc4df4d43c35057c44e1e198238d
Iva Jovanovic <ivajovanovic50@gmail.com>
2026-05-14 07:07:04 +0200

    add vectorization analysis notebook

--- vectorization/bow.py
+++ vectorization/bow.py
@@ -1,14 +1,20 @@
 from sklearn.feature_extraction.text import CountVectorizer
+import os
+import pickle
 
 def get_bow_vectorizer(ngram_range: tuple = (1, 1)): #testirati sa razlicitim kombinacijama n-grama
     return CountVectorizer(ngram_range=ngram_range)
 
 def vectorize(df, vectorizer):
-    # Spajamo tokene nazad u string jer sklearn ocekuje string
+    # tokeni -> string jer bow ocekuje stringove
     texts = df['cleaned'].apply(lambda tokens: ' '.join(tokens))
+
     # fit pravi recnik jedinstvenih reci
     # transform pretvara svaki red- recenziju u vektor duzine erecnika
     X = vectorizer.fit_transform(texts)
-    # X - matrica, redovi su recenzije a kolone jedinstvene reci
+
+    #os.makedirs("results", exist_ok=True)
+    #with open("results/bow_vectors.pkl", "wb") as f:
+    #    pickle.dump(X, f)
 
     return X
